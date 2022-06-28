6b79746e95e298008b431c853141061af620a654
Dmytro Bagrii <dimich.dmb@gmail.com>
2022-06-28 10:23:06 +0300

    Build script

--- make.bat
+++ make.bat
@@ -1,5 +1,3 @@
 @echo off
-c:\compile\bin\tasm /r /l sphere.asm
-c:\compile\bin\tlink /t /n /x sphere.obj
-rem c:\compile\bin\tasm unpack.asm
-rem c:\compile\bin\tlink /t unpack.obj
+c:\bin\tasm /r /l sphere.asm
+c:\bin\tlink /t /n /x sphere.obj
